2c1a37c0f7c3c40b6aa3d7133a2956a34198ff2b
unknown <mbp818@gmail.com>
2022-01-29 22:31:07 -0800

    pls

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
 const db = knex({
   // Enter your own database information here based on what you created
